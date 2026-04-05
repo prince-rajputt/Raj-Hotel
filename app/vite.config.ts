67402b1abf8bdb1ce7fc34c853ee67b1da7e87bf
Prince Kumar <princekumarekc45@gmail.com>
2026-04-05 13:39:03 +0530

    Add GitHub Pages workflow

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Raj-Hotel/',
   plugins: [react()],
-  build: {
-    outDir: '../',
-    emptyOutDir: false,
-    assetsDir: 'app-assets',
-  },
 })
