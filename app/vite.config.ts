a48e0613aafa98672e42cbe30bf35ff880bcde1d
Prince Kumar <princekumarekc45@gmail.com>
2026-04-05 13:34:21 +0530

    Fix GitHub Pages deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Raj-Hotel/',
   plugins: [react()],
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+    assetsDir: 'app-assets',
+  },
 })
